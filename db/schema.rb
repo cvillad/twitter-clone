4efa6aab7389eec25e192dec9d1acf5e5a264ea0
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-08 15:27:41 -0500

    add home, login, singup pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_170226) do
+ActiveRecord::Schema.define(version: 2021_03_08_200343) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(version: 2021_03_08_170226) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "tweets", "users"
