85581816efdf7255ff66ddd7aa62a2d38bc23c95
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-10 14:36:30 -0500

    refactor views, add home layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  layout "home"
+
   def index
     redirect_to tweets_path if user_signed_in?
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  layout "follows", only: [:following, :followers]
   before_action :set_user
   before_action :set_current_user_followings, only: [:follow, :unfollow]
 
