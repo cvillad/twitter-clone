c7184fd858adbf9a44c0f1ec4857e58bddcaf555
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-11 15:49:44 -0500

    change follow/unfollow from post to put

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :tweets, only: [:index, :new, :create, :destroy]
   get "/:username", to: "users#show", as: "user"
-  post "/follow/:username", to: "users#follow", as: "user_follow"
-  post "/unfollow/:username", to: "users#unfollow", as: "user_unfollow"
+  put "/follow/:username", to: "users#follow", as: "user_follow"
+  put "/unfollow/:username", to: "users#unfollow", as: "user_unfollow"
   get "/:username/following", to: "users#following", as: "user_following"
   get "/:username/followers", to: "users#followers", as: "user_followers"
   get "/users/search", to: "users#index", as: "user_search"
