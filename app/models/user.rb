d0e050363e253e8c7244736e11848a5643059f42
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-15 09:21:03 -0500

    fix counter_cache for followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,13 @@ class User < ApplicationRecord
   has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
 
   # Will return an array of users who follow the user instance
-  has_many :followers, through: :received_follows, source: :follower
+  has_many :followers, through: :received_follows, source: :follower, counter_cache: :followers_count
 
   # returns an array of follows a user gave to someone else
   has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
   
   # returns an array of other users who the user has followed
-  has_many :followings, through: :given_follows, source: :followed_user
+  has_many :followings, through: :given_follows, source: :followed_user, counter_cache: :followings_count
 
   def login
     @login || self.username || self.email
